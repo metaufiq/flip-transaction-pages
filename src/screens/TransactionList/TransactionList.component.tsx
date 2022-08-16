49700cee75e1da093c2fad787f95f4a57789ecc1
muh.taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-16 21:44:42 +0800

    feat(transaction-list): update implementation useTransactions hook to handle searchInput

--- src/screens/TransactionList/TransactionList.component.tsx
+++ src/screens/TransactionList/TransactionList.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, ListRenderItem, View } from "react-native";
 
 import { Transaction } from "../../../index.types";
@@ -8,13 +8,15 @@ import useTransactions from "../../hooks/useTransactions/useTransactions.hooks";
 import { Params as UseTransactionsParams } from "../../hooks/useTransactions/useTransactions.hooks.types";
 import useTransactionsQuery from "../../hooks/useTransactionsQuery/useTransactionsQuery.hooks";
 import styles from "./TransactionList.component.styles";
-import { Props } from "./TransactionList.component.types";
+import { Props, SetSearchInput } from "./TransactionList.component.types";
 
 const _onPressTransactionCard = (props:Props, transaction: Transaction)=>()=>{
   props.navigation.push('TransactionDetail', {transaction})
 }
 
-const _onSearch = ()=>{}
+const _onSearch = (setSearchInput: SetSearchInput)=>(searchInput: string)=>{
+  setSearchInput(searchInput)
+}
 
 const _renderTransactionList:(props:Props)=>ListRenderItem<Transaction> = (props:Props) =>({item})=>(
   <TransactionCard transaction={item} key={item.id} onPress={_onPressTransactionCard(props, item)}/>
@@ -22,16 +24,18 @@ const _renderTransactionList:(props:Props)=>ListRenderItem<Transaction> = (props
 
 const TransactionList = (props: Props) =>{
   const {transactions : queryResult} = useTransactionsQuery();
+  const [searchInput, setSearchInput] = useState<string>();
 
   const useTransactionsParams:UseTransactionsParams = {
-    initialValue: queryResult
+    initialValue: queryResult,
+    searchInput
   }
   const {transactions} = useTransactions(useTransactionsParams);
 
   return (
     <View style={styles.container}>
       <View style={styles.searchInputContainer}>
-        <SearchInput onChange={_onSearch} placeholder='Cari nama, bank, atau nominal'/>
+        <SearchInput onChange={_onSearch(setSearchInput)} placeholder='Cari nama, bank, atau nominal'/>
       </View>
       
       <FlatList

01063c9fc720e5bdc700aa737879db0c7e66ad6b
muh.taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-16 21:44:19 +0800

    feat(transaction-list): update  useTransactions hook to handle searchInput

--- src/hooks/useTransactions/useTransactions.hooks.ts
+++ src/hooks/useTransactions/useTransactions.hooks.ts
@@ -1,18 +1,47 @@
 import { useEffect, useState } from "react";
 
-import { SetTransactionsList, TransactionsList } from "../../../index.types";
-import { Params } from "./useTransactions.hooks.types";
+import { SetTransactionsList, Transaction, TransactionsList } from "../../../index.types";
+import { FilterKey, Params } from "./useTransactions.hooks.types";
 
+
+const FILTER_KEYS:FilterKey[] = ['amount', 'beneficiary_name', 'sender_bank', 'beneficiary_bank'];
 const useInnit = (initialValue: Params['initialValue'], setTransactionList: SetTransactionsList)=>{
   useEffect(()=>{
     setTransactionList(initialValue)
   }, [initialValue]);
 }
 
-const useTransactions = ({initialValue}: Params)=>{
+const filterByKeys = (searchInput: string)=>(transaction:Transaction) => {
+  return FILTER_KEYS.some(key=> {
+    let data = transaction[key]
+    if (typeof data === 'number') {
+      data = data.toString()
+    }
+
+    const lowerCaseData = data.toLowerCase();
+    const lowerCaseSearchInput = searchInput.toLowerCase();
+
+    return lowerCaseData.includes(lowerCaseSearchInput)
+  })
+}
+
+const useUpdateTransaction = ({searchInput, initialValue}: Params, setTransactions: SetTransactionsList)=>{
+
+  useEffect(()=>{
+    
+    setTransactions(transactions=> {
+      if (!searchInput) return initialValue
+      return transactions.filter(filterByKeys(searchInput))
+    })
+  }, [searchInput])
+}
+
+const useTransactions = (params: Params)=>{
   const [transactions, setTransactions] = useState<TransactionsList>([]);
 
-  useInnit(initialValue, setTransactions)
+  useInnit(params.initialValue, setTransactions)
+
+  useUpdateTransaction(params, setTransactions)
 
   return {transactions}
 }
--- src/hooks/useTransactions/useTransactions.hooks.types.ts
+++ src/hooks/useTransactions/useTransactions.hooks.types.ts
@@ -1,5 +1,8 @@
-import { TransactionsList } from "../../../index.types";
+import { TransactionsList, Transaction } from "../../../index.types";
 
 export type Params = {
   initialValue: TransactionsList
+  searchInput?: string
 }
+
+export type FilterKey =  'amount' | 'beneficiary_name' | 'sender_bank' | 'beneficiary_bank';

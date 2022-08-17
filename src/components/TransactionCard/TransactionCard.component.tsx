1c772fe4353cd767ddac0065710d2794d44aa0e1
muh.taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-17 11:04:02 +0800

    feat(transaction-card-components): upgrade transaction card UI

--- src/components/TransactionCard/TransactionCard.component.tsx
+++ src/components/TransactionCard/TransactionCard.component.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { Text, TouchableHighlight, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { TransactionStatus } from "../../../index.types";
+import { colors } from "../../configs/themes";
+import { currencyFormatter, dateFormat } from "../../utils/index.utils";
 
 import styles from "./TransactionCard.component.styles";
 import { Props } from "./TransactionCard.component.types";
 
+const _getContainerStyle = (status: TransactionStatus)=>(
+  {borderLeftColor: status === "PENDING" ? colors.primary: colors.success}
+  )
+
 const TransactionCard = ({transaction, onPress}: Props)=>{
   return(
     <TouchableOpacity onPress={onPress}>
-      <View style={styles.container}>
+      <View style={[styles.container, _getContainerStyle(transaction.status)]}>
         <View>
-          <Text>{transaction.sender_bank} → {transaction.beneficiary_bank}</Text>
-          <Text>{transaction.beneficiary_name}</Text>
-          <Text>{transaction.amount+transaction.fee} • {transaction.completed_at}</Text>
+          <Text style={styles.bankInformation}>{transaction.sender_bank} → {transaction.beneficiary_bank}</Text>
+          <Text style={styles.beneficiaryName}>{transaction.beneficiary_name.toUpperCase()}</Text>
+          <Text style={styles.transferInformation}>{currencyFormatter(transaction.amount+transaction.fee)} • {dateFormat(transaction.completed_at)}</Text>
         </View>
         <View>
           <Text>{transaction.status}</Text>
